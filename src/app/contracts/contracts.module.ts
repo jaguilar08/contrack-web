d531d200540e0d852de42b3abd9c6dcbd5d41ace
dpliego <dpliego@dealergeek.com>
2022-08-02 13:13:08 -0400

    fix: contracts routing

---
+++ src/app/contracts/contracts.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ContractsRoutingModule } from './contracts-routing.module';
+import { SharedModule } from '../shared/shared.module';
+
+import { ContractDetailsDialogComponent } from './components/contract-details-dialog/contract-details-dialog.component';
+import { ContractEditDialogComponent } from './components/contract-edit-dialog/contract-edit-dialog.component';
+import { ContractDeleteDialogComponent } from './components/contract-delete-dialog/contract-delete-dialog.component';
+
+
+
+@NgModule({
+  declarations: [
+
+    ContractDetailsDialogComponent,
+    ContractEditDialogComponent,
+    ContractDeleteDialogComponent
+  ],
+  imports: [
+    CommonModule,
+    ContractsRoutingModule,
+    SharedModule
+  ]
+})
+export class ContractsModule { }
